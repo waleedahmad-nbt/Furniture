1fda392707b6698381d595c84756f0eb301643fe
abubakarnbt <abubakarnbt12@gmail.com>
2023-12-07 16:57:02 +0500

    assets

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -1,9 +1,35 @@
+import Link from "next/link";
+
 const Navbar = () => {
   return (
     <div className="bg-gray-400 text-white">
-      <div className="container">
+      <header className="border-b">
+        <div className="container">
+          <ul className="flex gap-3">
+            <li>
+              <Link href="#">
+                Track Order
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+                About Us
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+                FAQ
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </header>
         Navbar
-      </div>
     </div>
   )
 }
