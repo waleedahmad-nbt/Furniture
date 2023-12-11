0a76dce095c1ca59004268c15ea8f0460e2618d6
moeeznbt123 <moeeznbt@gmail.com>
2023-12-11 13:37:12 +0500

    fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
@@ -27,11 +27,10 @@ import starFill from "@/app/assets/icons/star_fill.svg";
 import Banner from "@/app/assets/banners/banner_02.png";
 
 export default function Home() {
-
   const tabs = ["Bedroom", "Dining Room", "Living Room"];
 
   const [activeTab, setActiveTab] = useState<string>(tabs[0]);
-
+  // test
   const cats = [
     { title: "Bedroom", image: mirror, quantity: "12" },
     { title: "Dining room", image: table, quantity: "12" },
@@ -39,7 +38,7 @@ export default function Home() {
     { title: "Kitchen", image: kitchen, quantity: "12" },
     { title: "Living room", image: chair, quantity: "12" },
     { title: "Bed room", image: bed, quantity: "12" },
-  ]
+  ];
 
   const products = [
     { name: "Rocket stool", images: [Pro2, kitchen, bed], priceWas: "27.90", priceNow: "18.80", quantity: 2, status: "sale" },
@@ -72,12 +71,25 @@ export default function Home() {
           <div className="flex -mx-[15px] Home_Banner">
             <div className="flex-[0_0_100%] md:flex-[0_0_50%] px-[15px]">
               <div className="text-gray-300">
-                <span className="text-[18px] font-medium">Trending Furniture</span>
-                <h1 className="text-[50px] font-medium">Great Furniture<br />Sale</h1>
-                <p className="border-l-[3px] border-gray-300 my-10 pl-5">Limited time<br />sale up to 30% off</p>
-                <Link href="#" className="flex items-center gap-3 bg-primary px-6 py-3 w-max text-white">
+                <span className="text-[18px] font-medium">
+                  Trending Furniture
+                </span>
+                <h1 className="text-[50px] font-medium">
+                  Great Furniture
+                  <br />
+                  Sale
+                </h1>
+                <p className="border-l-[3px] border-gray-300 my-10 pl-5">
+                  Limited time
+                  <br />
+                  sale up to 30% off
+                </p>
+                <Link
+                  href="#"
+                  className="flex items-center gap-3 bg-primary px-6 py-3 w-max text-white"
+                >
                   <span>Shop now</span>
-                  <Image src={arrowL} alt="icon"/>
+                  <Image src={arrowL} alt="icon" />
                 </Link>
               </div>
             </div>
@@ -85,24 +97,33 @@ export default function Home() {
           </div>
         </div>
       </div>
-      
+
       <div className="container my-10">
         <Features />
       </div>
 
       <div className="container my-10">
         <div className="text-center">
-          <h1 className="text-[38px] text-gray-300 font-bold">Our Categories</h1>
-          <p className="text-gray-200">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum.</p>
+          <h1 className="text-[38px] text-gray-300 font-bold">
+            Our Categories
+          </h1>
+          <p className="text-gray-200">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum.
+          </p>
         </div>
         <div className="grid grid-cols-6 gap-5 my-14">
           {cats?.map((item: any, index: number) => (
             <div key={index}>
               <div className="bg-cream h-[170px] flex items-center justify-center">
-                <Image src={item?.image} alt="product" className="mx-auto"/>
+                <Image src={item?.image} alt="product" className="mx-auto" />
               </div>
-              <h3 className="text-center font-medium text-gray-300 mt-3">{item?.title}</h3>
-              <p className="text-center text-gray-300 text-[14px]">{item?.quantity} Products</p>
+              <h3 className="text-center font-medium text-gray-300 mt-3">
+                {item?.title}
+              </h3>
+              <p className="text-center text-gray-300 text-[14px]">
+                {item?.quantity} Products
+              </p>
             </div>
           ))}
         </div>
@@ -110,17 +131,28 @@ export default function Home() {
 
       <div className="container my-10">
         <div className="flex items-center justify-between">
-          <h1 className="text-[38px] text-gray-300 font-bold">Best Selling Products</h1>
+          <h1 className="text-[38px] text-gray-300 font-bold">
+            Best Selling Products
+          </h1>
           <div className="flex items-center justify-between">
             {tabs?.map((item: any, index: number) => (
-              <button onClick={() => setActiveTab(item)} className={`px-5 text-gray-200 rounded-md ${activeTab === item ? "bg-gray-100" : ""}`} key={index}>
+              <button
+                onClick={() => setActiveTab(item)}
+                className={`px-5 text-gray-200 rounded-md ${
+                  activeTab === item ? "bg-gray-100" : ""
+                }`}
+                key={index}
+              >
                 {item}
               </button>
             ))}
           </div>
-          <Link href="#" className="flex items-center gap-3 bg-primary px-2 py-1 w-max text-white uppercase text-[14px]">
+          <Link
+            href="#"
+            className="flex items-center gap-3 bg-primary px-2 py-1 w-max text-white uppercase text-[14px]"
+          >
             <span>view all</span>
-            <Image src={arrowL} alt="icon"/>
+            <Image src={arrowL} alt="icon" />
           </Link>
         </div>
         <div className="w-full my-10">
@@ -257,5 +289,5 @@ export default function Home() {
         </div>
       </div>
     </>
-  )
+  );
 }
