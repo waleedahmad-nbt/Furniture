28358582ae42c62984affdb87ea97306deaf09ae
abubakarnbt <abubakarnbt12@gmail.com>
2023-12-16 18:55:31 +0500

    token fix

--- requestMethods.js
+++ requestMethods.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-const token = localStorage.getItem("token");
-console.log(token);
-var parsedToken;
-var TOKEN = "Dummy";
-if (token != undefined) {
-  parsedToken = JSON.parse(token);
-  console.log(parsedToken.token);
-  TOKEN = parsedToken.token;
-}
-// let token = "Dummy"
+// const token = localStorage.getItem("token");
+// console.log(token);
+// var parsedToken;
+// var TOKEN = "Dummy";
+// if (token != undefined) {
+//   parsedToken = JSON.parse(token);
+//   console.log(parsedToken.token);
+//   TOKEN = parsedToken.token;
+// }
+const TOKEN = "Dummy"
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 export const publicRequest = axios.create({
