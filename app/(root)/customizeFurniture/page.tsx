8154014b0a5010b54671976be981777185d0d58d
abubakarnbt <abubakarnbt12@gmail.com>
2023-12-23 12:14:05 +0500

    fixing issue

--- app/(root)/customizeFurniture/page.tsx
+++ app/(root)/customizeFurniture/page.tsx
@@ -2,22 +2,24 @@ import React from "react";
 import { FurniturePortfolio } from "../components";
 import { publicRequest } from "@/requestMethods";
 
+async function fetchData() {
+  try {
+    const response = await publicRequest.get("/portfolio");
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching portfolio:', error);
+    throw error;
+  }
+}
+
 const customizeFurniture = async () => {
-  const res = await publicRequest.get("/portfolio");
-  console.log(res.data);
+  const images: any = await fetchData();
 
   return (
     <>
-      <FurniturePortfolio response={res.data.data} />
+      <FurniturePortfolio response={images.data} />
     </>
   );
 };
 
 export default customizeFurniture;
-
-// export async function fetchData() {
-//   const response = await publicRequest.get("/portfolio");
-//   console.log(response.data);
-
-//   return response.data;
-// }
